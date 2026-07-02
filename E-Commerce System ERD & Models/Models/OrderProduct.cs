a42520cac8bcfdfa2e3832271cbff3a945360719
widaad alhandasi <widaadalhandasi@gmail.com>
2026-07-02 13:21:46 +0400

    update model class

--- E-Commerce System ERD & Models/Models/OrderProduct.cs
+++ E-Commerce System ERD & Models/Models/OrderProduct.cs
@@ -19,12 +19,14 @@ namespace E_Commerce_System_ERD___Models.Models
         public int quantity { get; set; }  //user input
 
         [Required]
+        [ForeignKey("Order")]
         public int orderId { get; set; }   //Composite Primary Key   //user input
 
         // Navigation Properties
         public  Order Order { get; set; }  //one order have many order product
 
         [Required]
+        [ForeignKey("product")]
         public int productId { get; set; }  //Composite Primary Key    //user input
         // Navigation Properties
         public Product product { get; set; }  //one Product have many order product
